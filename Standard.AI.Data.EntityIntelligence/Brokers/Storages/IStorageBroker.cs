ececc53cf5f156f8f70cfd4d449747c6fd9773f1
Milan Toncic <milan.toncic@zuehlke.com>
2023-07-08 08:51:28 +0800

    Initializing storage broker

---
+++ Standard.AI.Data.EntityIntelligence/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard.AI.Data.EntityIntelligence.Brokers.Storages
+{
+    internal interface IStorageBroker 
+    {
+        ValueTask<IEnumerable<dynamic>> Execute(string query); 
+    }
+}
---
+++ Standard.AI.Data.EntityIntelligence/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,36 @@
+﻿using EFxceptions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard.AI.Data.EntityIntelligence.Brokers.Storages
+{
+    internal class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            string connectionString = this.configuration.GetConnectionString(name: "DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public override void Dispose() { }
+
+        public ValueTask<IEnumerable<dynamic>> Execute(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
